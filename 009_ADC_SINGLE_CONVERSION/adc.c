4761822f8cff445fd77d8aff1c23a986c2b2976a
Hayati Kılıç <83482374+kingssize19@users.noreply.github.com>
2025-03-13 02:59:43 +0300

    Create adc.c

---
+++ 009_ADC_SINGLE_CONVERSION/adc.c
@@ -0,0 +1,51 @@
+#include "adc.h"
+
+
+void adc_init(void)
+{
+
+	/**********Configure the ADC GPIO pin**********/
+
+	/* Enable clock access to GPIOA */
+	RCC->AHB1ENR |= GPIOAEN;
+
+	/* Set the mode of PA1 to analog mode */
+	GPIOA->MODER |= (1U << 2);
+	GPIOA->MODER |= (1U << 3);
+
+
+	/**********Configure the ADC module**********/
+
+	/* Enable clock access to ADC */
+	RCC->APB2ENR |= ADC1EN;
+
+	/* Conversion sequence start */
+	ADC1->SQR3 = ADC1_CH1;
+	/* Conversion sequence length */
+	ADC1->SQR1 = ADC_SEQ_LEN_1;
+	/* Enable ADC module */
+	ADC1->CR2 |= CR2_ADON;
+
+}
+
+
+void start_conversion(void)
+{
+	/* Start adc conversion */
+	ADC1->CR2 |= CR2_SWSTART;
+}
+
+uint32_t adc_read(void)
+{
+	/* Wait for conversion to be complete */
+	while (!(ADC1->SR & SR_EOC));
+
+	/* Read converted result */
+	return (ADC1->DR);
+}
+
+
+
+
+
+
