e2a213cd6204cb12572b1aad07a38feb7512f5fb
Hayati Kılıç <83482374+kingssize19@users.noreply.github.com>
2025-03-16 03:13:06 +0300

    Update systick.h

--- 011_SYSTICK_DELAY/systick.h
+++ 011_SYSTICK_DELAY/systick.h
@@ -3,10 +3,10 @@
 
 #include "stm32f4xx.h"
 
-#define SYSTICK_LOAD_VAL			16000
-#define CTRL_ENABLE					(1U << 0)
-#define CTRL_CLKSRC					(1U << 2)
-#define CTRL_COUNTFLAG				(1U << 16)
+#define SYSTICK_LOAD_VAL       16000
+#define CTRL_ENABLE           (1U << 0)
+#define CTRL_CLKSRC           (1U << 2)
+#define CTRL_COUNTFLAG        (1U << 16)
 
 void SystickDelayMs(int n);
 
