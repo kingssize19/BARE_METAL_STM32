ee53fff1294bb80a02884c9a38f9b7cffc45cd98
Hayati Kılıç <83482374+kingssize19@users.noreply.github.com>
2025-03-18 22:31:12 +0300

    Create tim.c

---
+++ 013_TIMER_OUTPUT_COMPARE/tim.c
@@ -0,0 +1,61 @@
+#include "tim.h"
+
+
+#define TIM2EN		(1U << 0)
+#define CR1_CEN		(1U << 0)
+#define OC_TOGGLE   ((1U << 4) | (1U << 5))
+#define CCER_CC1E	(1U << 0)
+
+void tim2_1hz_init(void)
+{
+	/* Enable clock access to tim2 */
+	RCC->APB1ENR |= TIM2EN;
+
+	/* Set prescaler value */
+	TIM2->PSC = 1600 - 1;		// 16 000 000 / 1 600 = 10 000
+
+	/* Set auto-reload value */
+	TIM2->ARR = 10000 - 1;		// 10 000 / 10 000 = 1
+
+	/* Clear counter */
+	TIM2->CNT = 0;
+
+	/* Enable timer */
+	TIM2->CR1  |= CR1_CEN;
+
+}
+
+void tim2_pa5_output_compare(void)
+{
+	/* Enable clock access to GPIOA */
+	RCC->AHB1ENR |= (1U << 0);
+
+	/* Set PA5 mode to alternate function */
+	GPIOA->MODER &= ~(1U << 10);
+	GPIOA->MODER |= (1U << 11);
+
+	/* Set PA5 alternate function type to TIM2_CH1 (AF01)*/
+	GPIOA->AFR[0] |= (1U << 20);
+
+	/* Enable clock access to tim2 */
+	RCC->APB1ENR |= TIM2EN;
+
+	/* Set prescaler value */
+	TIM2->PSC = 1600 - 1;		// 16 000 000 / 1 600 = 10 000
+
+	/* Set auto-reload value */
+	TIM2->ARR = 10000 - 1;		// 10 000 / 10 000 = 1
+
+	/* Set output compare toggle mode */
+	TIM2->CCMR1 = OC_TOGGLE;
+
+	/* Enable tim2 ch1 in compare mode */
+	TIM2->CCER |= CCER_CC1E;
+
+	/* Clear counter */
+	TIM2->CNT = 0;
+
+	/* Enable timer */
+	TIM2->CR1  |= CR1_CEN;
+
+}
